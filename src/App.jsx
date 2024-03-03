88720dacb45305f461b6fa555f5f8f6363e9a4e8
karnakar <thallapallikarnakar@gmail.com>
2024-03-03 13:46:14 -0800

    inital checkin

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Player from "./componenets/Player";
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player  name="Player1" symbol="X" />
+          <Player  name="Player2" symbol="O" />
+        </ol>
+      </div>
+    </main>
+  );
+}
+
+export default App;
